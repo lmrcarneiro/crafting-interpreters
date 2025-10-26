60871c08fca488a13e91e6103b243759064bedf1
Luís Carneiro <lmrcarneiro@gmail.com>
2025-10-26 16:51:53 +0100

    Chapter 7 done

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -9,8 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,15 +29,15 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         // Indicate an error in the exit code
-        if (hadError)
-            System.exit(65);
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for (;;) {
+        for (; ; ) {
             System.out.println("> ");
             String line = reader.readLine();
             if (line == null)
@@ -55,7 +56,7 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -74,5 +75,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
